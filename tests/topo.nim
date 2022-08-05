f67e50e0a15255ace757db0e73a93649dfd33aca
Charles Blake <charlechaud@gmail.com>
2022-08-05 13:29:35 -0400

    Make this more nimble test friendly.

--- tests/topo.nim
+++ tests/topo.nim
@@ -5,5 +5,6 @@ let c: seq[seq[int]] = @[ @[1,2], @[2], @[0,3], @[] ]
 iterator nods(dg: seq[seq[int]]): int = (for i in 0..<dg.len: yield i)
 iterator kids(dg: seq[seq[int]], x: int): int = (for i in dg[x]: yield i)
 let gts = topoSort[int](g, g.len, nods, kids)
-echo isCyclic(g, g.len, nods, kids, gts), " ", gts
-echo isCyclic(c, c.len, nods, kids, topoSort[int](c, c.len, nods, kids))
+assert gts == @[5, 4, 2, 3, 1, 0]
+assert isCyclic(g, g.len, nods, kids, gts) == false
+assert isCyclic(c, c.len, nods, kids, topoSort[int](c, c.len, nods, kids))
