bb3b33b4f5be7509362fa20bde55ecc844245bd6
Charles Blake <charlechaud@gmail.com>
2022-10-08 07:36:13 -0400

    Add replacement for cligen/examples/conncomp using APIs of this package.
    It's not identical in time/space costs since, to fit with the DG API, it
    does another pass, de-duplicating some edges in a HashSet of dest nodes.
    Even so, the algo is fast enough that parsing probably dominates cost for
    this particular utility program.

---
+++ util/conncomp.nim
@@ -0,0 +1,41 @@
+import tables, sets, grAlg, cligen
+
+proc id*[T](ids: var Table[T,SomeInteger], obs: var seq[T], ob: T): SomeInteger=
+  ## Get dense integer id for maybe-done `ob`, updating `ids` & `obs`. `tables`?
+  try:
+    result = ids[ob]                    # Already known => done
+  except:                               # Put into ob->id & id->ob
+    result = obs.len
+    ids[ob] = result
+    obs.add ob
+
+proc conncomp(idelim='\t', odelim="\t", n=1024, e=4096) =
+  ## Print connected components of the (undirected) graph on stdin as lines of
+  ## `idelim` edges.  Output is lines of `odelim`-delimited token clusters.
+  var ids   = initTable[string, int](n)         # token -> vertex int id number
+  var toks  = newSeqOfCap[string](n)            # vertex int id -> token
+  var edges = initTable[int, HashSet[int]](e)   # digraph as id -> destIds
+  let empty = initHashSet[int](e div n)
+
+  for ln in lines(stdin):                       # Parse input, assign node ids,
+    let cs = ln.split(idelim)                   #..and load up `edges`.
+    edges.mgetOrPut(ids.id(toks, cs[0]), empty).incl(
+                    ids.id(toks, cs[1]))
+
+  iterator nodes(edges: Table[int, HashSet[int]]): int =
+    for i in 0 ..< toks.len: yield i
+
+  iterator dests(edges: Table[int, HashSet[int]], x: int): int =
+    for d in edges.getOrDefault(x, empty): yield d
+
+  for c in edges.unDirCompons(toks.len + 1, nodes, dests).values:
+    for i, e in c:
+      stdout.write toks[e]
+      if i < c.len - 1:
+        stdout.write odelim
+    stdout.write "\n"
+
+dispatch conncomp, help={"idelim": "edge delimiter",
+                         "odelim": "in-cluster delimiter",
+                         "n"     : "guess at num of unique token nodes",
+                         "e"     : "guess at num of edges"}
