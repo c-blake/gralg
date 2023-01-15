4aa24b27f7caa005b4a99ff75d2359e562467101
Charles Blake <charlechaud@gmail.com>
2023-01-15 11:55:07 -0500

    Silence (bogus) unused warning.

--- tests/transClosure.nim
+++ tests/transClosure.nim
@@ -3,7 +3,7 @@ import grAlg
 
 let g = [ @[1,2], @[2], @[0,3], @[3]]
 
-iterator nods(dg: auto): int = (for i in 0..<dg.len: yield i)
+iterator nods(dg: auto): int {.used.} = (for i in 0..<dg.len: yield i)
 
 iterator kids[R,T](dg: array[R,seq[T]], x: int): T = (for e in dg[x]: yield e)
 
